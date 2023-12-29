84755d78aa6791a16c9651380b38e8f29aa3e149
abhishekmukati <abhishekmukati2017@gmail.com>
2023-12-29 18:20:36 +0530

    typeScript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Books from './component/Book';
+
+function App() {
+  return (
+    <div className="App">
+      <Books />
+    </div>
+  );
+}
+
+export default App;
